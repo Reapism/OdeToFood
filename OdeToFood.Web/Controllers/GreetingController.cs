fe7da72a345dc60c6a2f9c1d86ef7b263315d522
Anthony Jaghab <reapsprgm@gmail.com>
2019-09-24 16:08:16 -0400

    Using QueryString for the GET greeting controller.

--- OdeToFood.Web/Controllers/GreetingController.cs
+++ OdeToFood.Web/Controllers/GreetingController.cs
@@ -1,9 +1,5 @@
 ﻿using OdeToFood.Web.Models;
-using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace OdeToFood.Web.Controllers
@@ -11,10 +7,12 @@ namespace OdeToFood.Web.Controllers
     public class GreetingController : Controller
     {
         // GET: Greeting
-        public ActionResult Index()
+        public ActionResult Index(string name)
         {
-            var model = new GreetingViewModel();
-            model.Message = ConfigurationManager.AppSettings["message"];
+            GreetingViewModel model = new GreetingViewModel
+            {
+                Message = name ?? ConfigurationManager.AppSettings["message"]
+            };
             return View(model);
         }
     }
